dc2360847916e60fae7722869fa1dc2678e31693
Lora Zubić <lamia.lora@gmail.com>
2026-09-16 15:24:32 +0200

    project setup

---
+++ StreamApp/StreamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamAppApp.swift
+//  StreamApp
+//
+//  Created by Lora Zubić on 16.09.2026..
+//
+
+import SwiftUI
+
+@main
+struct StreamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
